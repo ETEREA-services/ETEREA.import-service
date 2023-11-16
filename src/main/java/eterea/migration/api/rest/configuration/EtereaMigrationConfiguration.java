87d7e3281f39e169f7541422b9f9cd87543ccc18
dqmdz <daniel.quinterospinto@gmail.com>
2023-11-16 10:47:01 -0300

    Habilita scheduling en la configuración

--- src/main/java/eterea/migration/api/rest/configuration/EtereaMigrationConfiguration.java
+++ src/main/java/eterea/migration/api/rest/configuration/EtereaMigrationConfiguration.java
@@ -2,8 +2,10 @@ package eterea.migration.api.rest.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableJpaAuditing
+@EnableScheduling
 public class EtereaMigrationConfiguration {
 }
